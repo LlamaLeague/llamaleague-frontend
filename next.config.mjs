6454f7baead2f7c1717b87b9e22361ced6b65621
Jafet De La Piedra <eliusblack68@gmail.com>
2026-03-21 13:57:04 -0500

    fix: eliminar Render, todo en Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `https://llamaleague-api.onrender.com/api/:path*`,
-      },
-    ]
-  },
 }
 
 export default nextConfig
